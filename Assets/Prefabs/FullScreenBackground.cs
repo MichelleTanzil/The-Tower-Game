9bbb9fcf33a1e40402a385d0c4aa3c91106a0111
Michelle Tanzil <mtanzil01@gmail.com>
2019-12-14 22:12:19 -0800

    added player movement (left and right)

---
+++ Assets/Prefabs/FullScreenBackground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenBackground : MonoBehaviour
+{
+
+  void Start()
+  {
+    Resize();
+  }
+
+
+
+  void Resize()
+  {
+    SpriteRenderer sr = GetComponent<SpriteRenderer>();
+    if (sr == null) return;
+
+    transform.localScale = new Vector3(1, 1, 1);
+
+    float width = sr.sprite.bounds.size.x;
+    float height = sr.sprite.bounds.size.y;
+
+    double variable = Camera.main.orthographicSize * 4.0;
+    float worldScreenHeight = (float)variable;
+    float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+    transform.localScale = new Vector2(worldScreenWidth / width, worldScreenHeight / height);
+
+  }
+}
