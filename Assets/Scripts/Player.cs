0fc8c4e523fa2dbc819b05f57d586ef05036bf1e
Michelle Tanzil <mtanzil01@gmail.com>
2019-12-15 11:08:51 -0800

    added player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,26 +5,55 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    //Config
     [SerializeField] float runSpeed = 5f;
     Rigidbody2D myRigidBody;
+    [SerializeField] float jumpSpeed = 5f;
+
+    //State
+    bool isAlive = true;
+
+    //Cached
+    Animator myAnimator;
+    Collider2D myColldier2D;
     // Start is called before the first frame update
+
+    //Message then methods
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myColldier2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
     }
 
     private void Run()
     {
+        if (!myColldier2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+            return;
+
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //vslue is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool horizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", horizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
     private void FlipSprite()
     //if the player is moving horizontally
